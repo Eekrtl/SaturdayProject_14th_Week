3baf89917434b7017746e97748d89d492f1d596e
Naime Kaymaz <119721390+Naime922@users.noreply.github.com>
2023-03-09 15:16:45 -0700

    tc5 created and done

--- src/test/java/UtilityClass/DriverClass.java
+++ src/test/java/UtilityClass/DriverClass.java
@@ -10,41 +10,43 @@ import java.awt.*;
 
 public class DriverClass {
     public static WebDriver driver;
+
     static {
         WebDriverManager.chromedriver().setup();
-        driver=new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
-//        login();
+        login();
     }
 
-    public static void quitDriver(int time){
+    public static void quitDriver(int time) {
         try {
-            Thread.sleep(time*1000);
+            Thread.sleep(time * 1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
         driver.quit();
     }
-    public static void wait(int second){
+
+    public static void wait(int second) {
         try {
-            Thread.sleep(second*1000);
-        }catch (InterruptedException e){
-            throw  new RuntimeException(e);
+            Thread.sleep(second * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
 
     }
-//    public static void login(){
-//        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-//        wait(2);
-//
-//        WebElement username= driver.findElement(By.name("username"));
-//        username.sendKeys("Admin");
-//
-//        WebElement password= driver.findElement(By.name("password"));
-//        password.sendKeys("admin123");
-//
-//        WebElement login=driver.findElement(By.cssSelector("button[type='submit']"));
-//        login.click();
+
+    public static void login() {
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        wait(2);
+
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("Admin");
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("admin123");
+        WebElement login = driver.findElement(By.cssSelector("button[type='submit']"));
+        login.click();
     }
-//}
+}
+
 
